b98acc0d5d35a7a278906778f1c4cfd037685635
wenxchn <wenx.chn@gmail.com>
2021-05-04 23:56:05 -0400

    Code quality, Fixed minor issue with sending data

--- server.js
+++ server.js
@@ -3,17 +3,6 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 
-const getReviews = async function(gameTitle) {
-  var editedTitle = gameTitle.split(" ").join("+");
-
-  const searchURL = `https://store.steampowered.com/search/?term=${editedTitle}`
-
-  console.log(searchURL)
-
-  const searchRES = await axios.get(searchURL)
-  // console.log(searchRES)
-  return searchRES
-}
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
@@ -25,28 +14,20 @@ app.use(function (req, res, next) {
 app.get('/:title', async function (req, res) {
   var gameTitle = req.params.title;
   var editedTitle = gameTitle.split(" ").join("+");
-
   const searchURL = `https://store.steampowered.com/search/?term=${editedTitle}`
-
-  console.log(searchURL)
-
-  const {data} = await axios.get(searchURL)
-
-  // var html = await getReviews(gameTitle)
-  // console.log(html)
+  const { data } = await axios.get(searchURL)
   const $ = cheerio.load(data)
   var webLink = '';
+
   $('div #search_resultsRows').first().find('a').first().each((i, link) => {
     webLink = link.attribs.href;
-
   });
+
   const regex = /app\/\d*/
   const matched = webLink.match(regex).toString()
-  // console.log(matched)
   const reviewsURL = `https://steamcommunity.com/${matched}/reviews/?browsefilter=toprated&snr=1_5_100010_`
-  console.log(reviewsURL)
-  const {reviewsHtml} = await axios.get(searchURL)
-  res.data = reviewsHtml
+  const reviewsHtml = await axios.get(reviewsURL).then(res => res.data);
+  res.send(reviewsHtml)
 })
 
 app.listen(5000)
