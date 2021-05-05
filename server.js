f73339b625ca7cc7e3d7cc291c1f9643292c5af2
nonoseer <huliumccall@gmail.com>
2021-05-05 00:17:13 -0400

    added multiple page fetching support

--- server.js
+++ server.js
@@ -3,17 +3,6 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 
-const getReviews = async function(gameTitle) {
-  var editedTitle = gameTitle.split(" ").join("+");
-
-  const searchURL = `https://store.steampowered.com/search/?term=${editedTitle}`
-
-  console.log(searchURL)
-
-  const searchRES = await axios.get(searchURL)
-  // console.log(searchRES)
-  return searchRES
-}
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
@@ -42,11 +31,19 @@ app.get('/:title', async function (req, res) {
   });
   const regex = /app\/\d*/
   const matched = webLink.match(regex).toString()
-  // console.log(matched)
   const reviewsURL = `https://steamcommunity.com/${matched}/reviews/?browsefilter=toprated&snr=1_5_100010_`
+
   console.log(reviewsURL)
-  const {reviewsHtml} = await axios.get(searchURL)
-  res.data = reviewsHtml
+  var reviewsHtml = await axios.get(reviewsURL).then(res => res.data)
+  for (i = 1; i <= 100; i++) {
+    const p = i + 1;
+    const pageUrl = `https://steamcommunity.com/app/1158310/homecontent/?userreviewscursor=AoIIP28jV3rJvKAC&userreviewsoffset=${i}0&p=${p}&workshopitemspage=${p}&readytouseitemspage=${p}&mtxitemspage=${p}&itemspage=${p}&screenshotspage=${p}&videospage=${p}&artpage=${p}&allguidepage=${p}&webguidepage=${p}&integratedguidepage=${p}&discussionspage=${p}&numperpage=10&browsefilter=toprated&browsefilter=toprated&l=english&appHubSubSection=10&filterLanguage=default&searchText=&maxInappropriateScore=50&forceanon=1`
+    console.log(`fetching page: ${p}`)
+    const pageHtml = await axios.get(pageUrl).then(res => res.data)
+    reviewsHtml = reviewsHtml + pageHtml
+  }
+
+  res.send(reviewsHtml)
 })
 
 app.listen(5000)
